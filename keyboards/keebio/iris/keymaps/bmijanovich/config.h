68faae352b2a1e643d1dda5b088b3e5436983aed
bmijanovich <bmijanovich@gmail.com>
2021-06-07 10:25:28 -0400

    Mod/shift on space/enter

--- keyboards/keebio/iris/keymaps/bmijanovich/config.h
+++ keyboards/keebio/iris/keymaps/bmijanovich/config.h
@@ -19,6 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // #define USE_I2C
 #define EE_HANDS
+
 #define MASTER_RIGHT
 
 #define USB_POLLING_INTERVAL_MS 1
--- keyboards/keebio/iris/keymaps/bmijanovich/keymap.c
+++ keyboards/keebio/iris/keymaps/bmijanovich/keymap.c
@@ -4,16 +4,6 @@
 bool is_cmd_tab_active = false;
 uint16_t cmd_tab_timer = 0;
 
-//Mac text editing shortcuts
-#define KC_GL LGUI(KC_LEFT)
-#define KC_GR LGUI(KC_RGHT)
-#define KC_GU LGUI(KC_UP)
-#define KC_GD LGUI(KC_DOWN)
-
-//Shift lock
-#define KC_LSCL LSFT_T(KC_CAPS)
-#define KC_RSCL RSFT_T(KC_CAPS)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT(
@@ -22,11 +12,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
     //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-       LT(2,KC_ESC), KC_A, KC_S,  KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+       LT(2, KC_ESC), KC_A, KC_S, KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-       KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC,           RGB_TOG, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
+       KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LT(1, KC_SPC),    RGB_TOG, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
     //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                      KC_LGUI, MO(1),   KC_SPC,                    KC_ENT,  KC_RCTL, KC_RALT
+                                      KC_LOPT, KC_LGUI, LT(1, KC_SPC),             RSFT_T(KC_ENT), KC_RGUI, KC_RCTL
                                   // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
     ),
 
@@ -34,29 +24,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
        KC_PSCR, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
     //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-       XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, KC_LBRC,                            KC_RBRC, KC_GL,   KC_UP,   KC_GR,   KC_GU,   KC_F11,
+       XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, KC_LBRC,                            KC_RBRC, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_F11,
     //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-       _______, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, KC_EQL,                             KC_MINS, KC_LEFT, KC_DOWN, KC_RGHT, KC_GD,   KC_F12,
+       LT(2, KC_CAPS), XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, KC_EQL,                      KC_MINS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_F12,
     //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-       KC_LSCL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSCL,
+       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,          _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
     //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                      _______, _______, XXXXXXX,                   XXXXXXX, _______, _______
+                                      _______, _______, _______,                   _______, _______, _______
                                   // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
     ),
 
     [2] = LAYOUT(
     //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-       KC_PSCR, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
+       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-       XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, KC_LBRC,                            KC_RBRC, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_F11,
+       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-       _______, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, KC_EQL,                             KC_MINS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_F12,
+       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-       KC_LSCL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSCL,
+       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,          _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
     //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                      _______, _______, XXXXXXX,                   XXXXXXX, _______, _______
+                                      _______, _______, _______,                   _______, _______, _______
                                   // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
-    ),
+    )
 };
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
