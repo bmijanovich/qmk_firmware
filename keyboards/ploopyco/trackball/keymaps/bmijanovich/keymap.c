ee6e4bceb67b799bf7d6c5cfa769d684990fbc84
bmijanovich <bmijanovich@gmail.com>
2021-07-25 14:49:52 -0400

    Buggy multi-tap dance Ploopy keymap
    This code has a bug. If you roll through any two of the tap dance keys, you'll get stuck in the held state for the second key. This will leave a layer or DRAG_SCROLL held on when no keys are pressed. I think this is a bug in QMK.

--- keyboards/ploopyco/trackball/keymaps/bmijanovich/keymap.c
+++ keyboards/ploopyco/trackball/keymaps/bmijanovich/keymap.c
@@ -8,44 +8,26 @@ typedef enum {
     TD_SINGLE_TAP,
     TD_SINGLE_HOLD,
     TD_DOUBLE_TAP
-} td_state_t;
+} td_action_t;
 
-typedef struct {
-    bool is_press_action;
-    td_state_t state;
-} td_tap_t;
-
-//Supports single tap, double tap, and single hold. Favors instant hold when interrupted.
-td_state_t single_hold_double_tap_mouse_dance(qk_tap_dance_state_t *state) {
+//Favors instant hold when interrupted.
+td_action_t get_tap_dance_action(qk_tap_dance_state_t *state) {
     if (state->count == 1) {
-        if (!state->pressed) return TD_SINGLE_TAP;
-        else return TD_SINGLE_HOLD;
+        if (state->pressed) return TD_SINGLE_HOLD;
+        else return TD_SINGLE_TAP;
     }
     else if (state->count == 2) return TD_DOUBLE_TAP;
     else return TD_UNKNOWN;
 }
 
-//Supports single tap and single hold. Favors instant hold when interrupted.
-td_state_t single_hold_single_tap_mouse_dance(qk_tap_dance_state_t *state) {
-    if (state->count == 1) {
-        if (!state->pressed) return TD_SINGLE_TAP;
-        else return TD_SINGLE_HOLD;
-    }
-    else return TD_UNKNOWN;
-}
 
 
-
-
-static td_tap_t lt_btn2_ent_tap_state = {
-    .is_press_action = true,
-    .state = TD_NONE
-};
+static td_action_t lt_btn2_ent_td_action = TD_NONE;
 
 //Send right click on single tap, Enter on double tap, and switch to layer 1 when held
 void lt_btn2_ent_finished(qk_tap_dance_state_t *state, void *user_data) {
-    lt_btn2_ent_tap_state.state = single_hold_double_tap_mouse_dance(state);
-    switch (lt_btn2_ent_tap_state.state) {
+    lt_btn2_ent_td_action = get_tap_dance_action(state);
+    switch (lt_btn2_ent_td_action) {
         case TD_SINGLE_TAP:
             tap_code16(KC_BTN2);
             break;
@@ -63,29 +45,40 @@ void lt_btn2_ent_finished(qk_tap_dance_state_t *state, void *user_data) {
 //Turn off layer 1 if the button was held
 void lt_btn2_ent_reset(qk_tap_dance_state_t *state, void *user_data) {
     // If the key was held down and now is released then switch off the layer
-    if (lt_btn2_ent_tap_state.state == TD_SINGLE_HOLD) {
+    if (lt_btn2_ent_td_action == TD_SINGLE_HOLD) {
         layer_off(1);
     }
-    lt_btn2_ent_tap_state.state = TD_NONE;
+    lt_btn2_ent_td_action = TD_NONE;
 }
 
 
 
 
-static td_tap_t dragscroll_back_tap_state = {
-    .is_press_action = true,
-    .state = TD_NONE
+static keyrecord_t btn4_record = {
+    .event = {
+        .key = {
+            .col = 3,
+            .row = 0
+            },
+        .pressed = false,
+        .time = 0
+    },
+    .tap = {0}
 };
+
+static td_action_t dragscroll_back_td_action = TD_NONE;
 
 //Send back on single tap and enable drag scroll when held
 void dragscroll_back_finished(qk_tap_dance_state_t *state, void *user_data) {
-    dragscroll_back_tap_state.state = single_hold_single_tap_mouse_dance(state);
-    switch (dragscroll_back_tap_state.state) {
+    dragscroll_back_td_action = get_tap_dance_action(state);
+    switch (dragscroll_back_td_action) {
         case TD_SINGLE_TAP:
             tap_code16(KC_BTN4);
             break;
         case TD_SINGLE_HOLD:
-            register_code16(DRAG_SCROLL);
+            btn4_record.event.pressed = true;
+            btn4_record.event.time = timer_read();
+            process_record_kb(DRAG_SCROLL, &btn4_record);
             break;
         default:
             break;
@@ -95,24 +88,23 @@ void dragscroll_back_finished(qk_tap_dance_state_t *state, void *user_data) {
 //Turn off drag scroll if the button was held
 void dragscroll_back_reset(qk_tap_dance_state_t *state, void *user_data) {
     // If the key was held down and now is released then switch off the layer
-    if (dragscroll_back_tap_state.state == TD_SINGLE_HOLD) {
-        unregister_code16(DRAG_SCROLL);
+    if (dragscroll_back_td_action == TD_SINGLE_HOLD) {
+        btn4_record.event.pressed = false;
+        btn4_record.event.time = timer_read();
+        process_record_kb(DRAG_SCROLL, &btn4_record);
     }
-    dragscroll_back_tap_state.state = TD_NONE;
+    dragscroll_back_td_action = TD_NONE;
 }
 
 
 
 
-static td_tap_t lt_fwd_tap_state = {
-    .is_press_action = true,
-    .state = TD_NONE
-};
+static td_action_t lt_fwd_td_action = TD_NONE;
 
 //Send forward on single tap and switch to layer 2 when held
 void lt_fwd_finished(qk_tap_dance_state_t *state, void *user_data) {
-    lt_fwd_tap_state.state = single_hold_single_tap_mouse_dance(state);
-    switch (lt_fwd_tap_state.state) {
+    lt_fwd_td_action = get_tap_dance_action(state);
+    switch (lt_fwd_td_action) {
         case TD_SINGLE_TAP:
             tap_code16(KC_BTN5);
             break;
@@ -127,10 +119,10 @@ void lt_fwd_finished(qk_tap_dance_state_t *state, void *user_data) {
 //Turn off layer 2 if the button was held
 void lt_fwd_reset(qk_tap_dance_state_t *state, void *user_data) {
     // If the key was held down and now is released then switch off the layer
-    if (lt_fwd_tap_state.state == TD_SINGLE_HOLD) {
+    if (lt_fwd_td_action == TD_SINGLE_HOLD) {
         layer_off(2);
     }
-    lt_fwd_tap_state.state = TD_NONE;
+    lt_fwd_td_action = TD_NONE;
 }
 
 
@@ -175,7 +167,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 /* TODO:
-  * Drag scroll on hold button 4
+  * Figure out why drag scroll gets stuck sometimes when using layers
   * Scroll wheel horizontal scroll when drag scroll enabled
   * Remove RESET if not needed
+  * Adjust tapping term per key
 */
