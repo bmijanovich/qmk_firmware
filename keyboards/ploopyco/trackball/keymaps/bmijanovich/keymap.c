8f4c3676839c29bb198f72de84f49061ece89146
bmijanovich <bmijanovich@gmail.com>
2021-07-28 10:44:25 -0400

    Cleaned up code and added another layer

--- keyboards/ploopyco/trackball/keymaps/bmijanovich/keymap.c
+++ keyboards/ploopyco/trackball/keymaps/bmijanovich/keymap.c
@@ -1,15 +1,71 @@
 #include QMK_KEYBOARD_H
 
-//Custom keycode for enabling rolling off layer and DPI_CONFIG button
+// Custom keycode for enabling rolling off layer and DPI_CONFIG button
 enum {
-    DPI_TOG = PLOOPY_SAFE_RANGE
+    DPI_TOG = PLOOPY_SAFE_RANGE,
 };
 
-// Utility function keycodes
+// Mac utility function keycodes
 #define KC_LS LCTL(KC_LEFT) // Move leftward one space
 #define KC_RS LCTL(KC_RGHT) // Move rightward one space
-#define KC_MS KC_F8 // Activate Mission Control
+#define KC_MS KC_F3 // Activate Mission Control
+#define KC_DT KC_F5 // Show desktop
 #define KC_SS LSFT(LCMD(KC_4)) // Take a screenshot
+
+// Track drag scrolling state
+static bool drag_scroll_active = false;
+
+
+
+
+/* Functions for sending custom keycodes
+
+QMK functions can't register custom keycodes, but we can setup a keyrecord_t and call process_record_kb() directly.
+
+Unknowns:
+    * Do we need to set the column and row for each keycode?
+    * Could reusing the same keyrecord_t struct cause problems with keycodes clobbering each other?
+*/
+// Dummy keyrecord_t for hooking process_record_kb() with custom keycodes
+static keyrecord_t dummy_record = {
+    .event = {
+        .key = {
+            .col = 0,
+            .row = 0,
+            },
+        .pressed = false,
+        .time = 0,
+    },
+    .tap = {0},
+};
+
+// Setup dummy_record for process_record_kb()
+void setup_dummy_record(uint8_t col, uint8_t row, bool pressed) {
+    dummy_record.event.key.col = col;
+    dummy_record.event.key.row = row;
+    dummy_record.event.pressed = pressed;
+    dummy_record.event.time = timer_read();
+}
+
+// Register a custom keycode with process_record_kb()
+void register_custom_keycode(uint16_t keycode, uint8_t col, uint8_t row) {
+    setup_dummy_record(col, row, true);
+    process_record_kb(keycode, &dummy_record);
+}
+
+// Unregister a custom keycode with process_record_kb()
+void unregister_custom_keycode(uint16_t keycode, uint8_t col, uint8_t row) {
+    setup_dummy_record(col, row, false);
+    process_record_kb(keycode, &dummy_record);
+}
+
+// Tap a custom keycode with process_record_kb()
+void tap_custom_keycode(uint16_t keycode, uint8_t col, uint8_t row) {
+    register_custom_keycode(keycode, col, row);
+    wait_ms(10);
+    unregister_custom_keycode(keycode, col, row);
+}
+/* End functions for sending custom keycodes */
 
 
 
@@ -28,14 +84,23 @@ typedef enum {
     TD_UNKNOWN,
     TD_SINGLE_TAP,
     TD_DOUBLE_TAP,
+    TD_TRIPLE_TAP,
     TD_SINGLE_HOLD,
 } td_action_t;
 
-//Suports single/double/triple taps and single hold. Favors instant hold when interrupted.
+// Suports single/double/triple taps and single hold. Favors instant hold when interrupted.
 td_action_t get_tap_dance_action(qk_tap_dance_state_t *state) {
     if (state->count == 1) return (state->pressed) ? TD_SINGLE_HOLD : TD_SINGLE_TAP;
     else if (state->count == 2) return TD_DOUBLE_TAP;
+    else if (state->count == 3) return TD_TRIPLE_TAP;
     else return TD_UNKNOWN;
+}
+
+// Reset and allow immediate repeat of a Tap Dance
+void hard_reset_tap_dance(qk_tap_dance_state_t *state) {
+    state->pressed = false;  // reset_tap_dance() will short circuit without this and we need it to complete
+    state->finished = true;  // Don't know if this is needed, but it can't hurt
+    reset_tap_dance(state);    
 }
 /* End shared Tap Dance configuration */
 
@@ -52,11 +117,10 @@ Mouse button 2 Tap Dance configuration
 static td_action_t btn2_td_action = TD_NONE;
 
 void btn2_td_tap(qk_tap_dance_state_t *state, void *user_data) {
-    if (state->count == 3) {
+    btn2_td_action = get_tap_dance_action(state);
+    if (btn2_td_action == TD_TRIPLE_TAP) {
         tap_code(KC_BSPC);
-        state->pressed = false;
-        state->finished = true;
-        reset_tap_dance(state);
+        hard_reset_tap_dance(state);
     }
 }
 
@@ -92,30 +156,15 @@ void btn2_td_reset(qk_tap_dance_state_t *state, void *user_data) {
 Mouse button 4 Tap Dance configuration
   * Single tap: Back (BTN4)
   * Double tap: Move leftward one space (KC_LS)
-  * Single hold: Enable trackball scrolling (DRAG_SCROLL)
+  * Single hold: Switch to layer 2 and enable trackball scrolling (DRAG_SCROLL)
 */
-static bool is_drag_scroll = false;
-
-static keyrecord_t btn4_record = {
-    .event = {
-        .key = {
-            .col = 3,
-            .row = 0
-            },
-        .pressed = false,
-        .time = 0
-    },
-    .tap = {0}
-};
-
 static td_action_t btn4_td_action = TD_NONE;
 
 void btn4_td_tap(qk_tap_dance_state_t *state, void *user_data) {
-    if (state->count == 2) {
+    btn4_td_action = get_tap_dance_action(state);
+    if (btn4_td_action == TD_DOUBLE_TAP) {
         tap_code16(KC_LS);
-        state->pressed = false;
-        state->finished = true;
-        reset_tap_dance(state);
+        hard_reset_tap_dance(state);
     }
 }
 
@@ -126,10 +175,9 @@ void btn4_td_finished(qk_tap_dance_state_t *state, void *user_data) {
             tap_code16(KC_BTN4);
             break;
         case TD_SINGLE_HOLD:
-            btn4_record.event.pressed = true;
-            btn4_record.event.time = timer_read();
-            process_record_kb(DRAG_SCROLL, &btn4_record);
-            is_drag_scroll = true;
+            layer_on(2);
+            register_custom_keycode(DRAG_SCROLL, 3, 0);
+            drag_scroll_active = true;
             break;
         default:
             break;
@@ -138,10 +186,9 @@ void btn4_td_finished(qk_tap_dance_state_t *state, void *user_data) {
 
 void btn4_td_reset(qk_tap_dance_state_t *state, void *user_data) {
     if (btn4_td_action == TD_SINGLE_HOLD) {
-        btn4_record.event.pressed = false;
-        btn4_record.event.time = timer_read();
-        process_record_kb(DRAG_SCROLL, &btn4_record);
-        is_drag_scroll = false;
+        layer_off(2);
+        unregister_custom_keycode(DRAG_SCROLL, 3, 0);
+        drag_scroll_active = false;
     }
     btn4_td_action = TD_NONE;
 }
@@ -154,16 +201,15 @@ void btn4_td_reset(qk_tap_dance_state_t *state, void *user_data) {
 Mouse button 5 Tap Dance configuration
   * Single tap: Forward (BTN5)
   * Double tap: Move rightward one space (KC_RS)
-  * Single hold: Switch to layer 2
+  * Single hold: Switch to layer 3
 */
 static td_action_t btn5_td_action = TD_NONE;
 
 void btn5_td_tap(qk_tap_dance_state_t *state, void *user_data) {
-    if (state->count == 2) {
+    btn5_td_action = get_tap_dance_action(state);
+    if (btn5_td_action == TD_DOUBLE_TAP) {
         tap_code16(KC_RS);
-        state->pressed = false;
-        state->finished = true;
-        reset_tap_dance(state);
+        hard_reset_tap_dance(state);
     }
 }
 
@@ -174,7 +220,7 @@ void btn5_td_finished(qk_tap_dance_state_t *state, void *user_data) {
             tap_code16(KC_BTN5);
             break;
         case TD_SINGLE_HOLD:
-            layer_on(2);
+            layer_on(3);
             break;
         default:
             break;
@@ -183,7 +229,7 @@ void btn5_td_finished(qk_tap_dance_state_t *state, void *user_data) {
 
 void btn5_td_reset(qk_tap_dance_state_t *state, void *user_data) {
     if (btn5_td_action == TD_SINGLE_HOLD) {
-        layer_off(2);
+        layer_off(3);
     }
     btn5_td_action = TD_NONE;
 }
@@ -209,7 +255,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, DPI_TOG, _______,
           _______, _______
     ),
-    [2] = LAYOUT( // Utility functions
+    [2] = LAYOUT( // Show desktop
+        _______, KC_DT, _______,
+          _______, _______
+    ),
+    [3] = LAYOUT( // Utility functions
         KC_BTN3, KC_SS, _______,
           RESET, _______
     )
@@ -217,7 +267,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Horizontal scrolling with wheel while DRAG_SCROLL enabled
 void process_wheel_user(report_mouse_t* mouse_report, int16_t h, int16_t v) {
-    if (is_drag_scroll) {
+    if (drag_scroll_active) {
         mouse_report->h = -v;
         pointing_device_set_report(*mouse_report);
         pointing_device_send();
@@ -229,29 +279,11 @@ void process_wheel_user(report_mouse_t* mouse_report, int16_t h, int16_t v) {
 }
 
 // Enable rolling off layer and DPI_CONFIG button
-static keyrecord_t btn3_record = {
-    .event = {
-        .key = {
-            .col = 1,
-            .row = 0
-            },
-        .pressed = false,
-        .time = 0
-    },
-    .tap = {0}
-};
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case DPI_TOG:
             if (!record->event.pressed) {
-                btn3_record.event.pressed = true;
-                btn3_record.event.time = timer_read();
-                process_record_kb(DPI_CONFIG, &btn3_record);
-                wait_ms(10);
-                btn3_record.event.pressed = false;
-                btn3_record.event.time = timer_read();
-                process_record_kb(DPI_CONFIG, &btn3_record);
+                tap_custom_keycode(DPI_CONFIG, 1, 0);
             }
             return false;
         default:
